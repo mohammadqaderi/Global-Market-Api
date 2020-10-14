7e6b4366586092d1f90c0a4dfd0a5d27270911e5
mohammadqaderi <mqaderi44@gmail.com>
2020-10-15 00:46:54 +0300

    forgot password update

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,24 @@ import { webPushInit } from './commons/functions/initalize-web-push';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
-    AppModule
+    AppModule,
+    {
+      cors: true,
+    },
   );
   webPushInit();
   SwaggerOptionsInit(app);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.use(function(req, res, next) { //allow cross origin requests
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, PUT, OPTIONS, DELETE, GET');
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
     next();
   });
   app.use((req, res, next) => {
@@ -21,7 +32,7 @@ async function bootstrap() {
       'Access-Control-Allow-Header',
       'Origin, X-Requested-With, Content-Type, Accept',
     );
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', '*');
     next();
   });
   const port: number = parseInt(`${process.env.PORT}`) || 3000;
